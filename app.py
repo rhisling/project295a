2dce781b4924a33dcbb7e77668b8ac4e77d5ec13
Aravindhan Elayakumar <aravindhansai@gmail.com>
2018-11-18 17:44:36 -0800

    Mocking data

--- app.py
+++ app.py
@@ -1,14 +1,13 @@
-import json, argparse, time
+import argparse
+import json
+
 
 import tensorflow as tf
+from flask import Flask, request, render_template
+
 from load import load_graph
 
-from flask import Flask, request
-from flask_cors import CORS
-
-
 app = Flask(__name__)
-
 
 @app.route('/')
 def hello_world():
@@ -25,6 +24,30 @@ def predict():
         params = json.loads(data)
         x_in = params['x']
 
+    data = {
+        "cpu_frequency": 13.56,
+        "cpu_gain": 15.56
+    }
+    x = []
+    for key, value in data:
+        x.append(value)
+
+    with tf.Session(graph=graph) as sess:
+        y_out = sess.run(y1, ({
+            x1: [x]
+        }))
+    json_data = json.dumps()
+    return
+
+
 
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--frozen_model_filename", default="results/frozen_model.pb", type=str,
+                        help="Frozen model file to import")
+    args = parser.parse_args()
+    graph = load_graph(args.frozen_model_filename)
+    x1 = graph.get_tensor_by_name('prefix/Input:0')
+    y1 = graph.get_tensor_by_name('prefix/add:0')
+
     app.run()
