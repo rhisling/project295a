f5d2513588648fe9824dd277d9bd763db22eced3
Aravindhan Elayakumar <aravindhansai@gmail.com>
2018-11-18 18:59:17 -0800

    Fixing the json

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import argparse
 import json
 import random
-import pandas as pd
+
 import tensorflow as tf
 from flask import Flask, request
 
@@ -16,26 +16,50 @@ def hello_world():
 
 @app.route('/api/predict', methods=['POST'])
 def predict():
-    data = request.data.decode("utf-8")
-    # if data == "":
-    #     params = request.form
-    #     x_in = json.loads(params['x'])
-    # else:
-    #     params = json.loads(data)
-    #     x_in = params['x']
-
+    data = request.data
+    dataDict = json.loads(data)
+    print(dataDict)
     data = {
         "cpu_frequency": 13.56,
-        "cpu_gain": 15.56
+        "no_network_connections": 2,
+        "time_spent_user": 15.56,
+        "time_spent_system": 23,
+        "time_spent_idle": 1,
+        "time_spent_io": 13,
+        "cpu_percentage": 3,
+        "ctx_switches": 3,
+        "interrupts": 3,
+        "software_interrupts": 23,
+        "system_calls": 3,
+        "percent_virtual_memory": 2,
+        "cached_memory_changed": 2,
+        "shared_memory_changed": 3,
+        "swap_percentage": 3,
+        "swap_in_bytes": 2,
+        "swap_out_bytes": 3,
+        "bytes_read": 2,
+        "bytes_write": 2,
+        "bytes_sent": 1,
+        "bytes_received": 2,
+        "no_processes": 3,
+        "rapl_value": 2
     }
-    x = []
-    x.append(data["cpu_frequency"])
-    x.append(data["cpu_gain"])
+
+    final_res = []
+    for i in dataDict:
+        x = list()
+        x.append(i["cpu_frequency"])
+        x.append(i["no_network_connection"])
+        x.append(i["time_spent_user"])
+        x.append(i["time_spent_system"])
+
+        final_res.append(x)
+
     my_randoms = [random.randrange(1, 101, 1) for _ in range(23)]
     ##y_out = [[]]
     with tf.Session(graph=graph) as sess:
         y_out = sess.run(y1, ({
-            x1: [my_randoms]
+            x1: final_res
         }))
 
     json_data = json.dumps({"power": y_out.tolist()})
